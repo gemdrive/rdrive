58026c3e272f2731e6b53897afb523ff5d5427bb
Anders Pitman <tapitman11@gmail.com>
2019-12-11 10:56:16 -0700

    Remove patchbay-specific stuff

--- cli.js
+++ cli.js
@@ -1,23 +1,13 @@
 #!/usr/bin/env node
-const process = require('process');
 
+const process = require('process');
+const http = require('http');
 const { createWebFSServer } = require('./index.js');
 
-const options = {};
+const httpServer = http.createServer();
 
-const patchbay = process.argv[2] === '--patchbay';
-if (patchbay) {
-  const http = require('patchbay-http');
-  options.httpServer = http.createServer();
-  options.httpServer.setPatchbayServer('https://patchbay.pub');
-  //srv.setPatchbayServer('http://localhost:9001');
-  options.httpServer.setPatchbayChannel('/webfs-test');
-  options.rootPath = '/req/webfs-test';
-}
-else {
-  const http = require('http');
-  options.httpServer = http.createServer();
-}
+const srv = createWebFSServer({
+  httpServer,
+});
 
-const srv = createWebFSServer(options);
 srv.start();
