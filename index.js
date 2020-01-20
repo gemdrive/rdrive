90f9aea5dafb67f9f76a76aa12d1f600860c3c52
Anders Pitman <tapitman11@gmail.com>
2020-01-20 13:44:28 -0700

    Move HTML directory renderer to separate file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const url = require('url');
+const { renderHtmlDir } = require('./render_html_dir.js');
 
 
 function createHandler(options) {
@@ -138,81 +139,7 @@ async function serveItem(req, res, rootPath, reqPath) {
   }
 }
 
-async function renderHtmlDir(req, res, rootPath, reqPath, fsPath) {
-  let filenames;
-  try {
-    filenames = await fs.promises.readdir(fsPath);
-  }
-  catch (e) {
-    res.statusCode = 500;
-    res.write("Error reading directory");
-    res.end();
-    return;
-  }
 
-  res.setHeader('Content-Type', 'text/html');
-
-  let pathParts = reqPath.split('/');
-
-  let listing;
-  if (pathParts.length > 1) {
-    pathParts.pop();
-    const parentUrl = rootPath + pathParts.join('/');
-    listing = `<a href=${parentUrl}>..[parent]</a> &#128193`;
-  }
-  else {
-    listing = '';
-  }
-
-  for (const filename of filenames) {
-
-    const url = rootPath + reqPath + '/' + filename;
-    const childFsPath = path.join(fsPath, filename);
-
-    let childStats;
-    try {
-      childStats = await fs.promises.stat(childFsPath);
-    }
-    catch (e) {
-      console.error("This one shouldn't happen");
-      console.error(e);
-      continue;
-    }
-
-    let link;
-    if (childStats.isDirectory()) {
-      link = `<a href=${url}>${filename}</a> &#128193`;
-    }
-    else {
-      link = `<a target='_blank' href=${url}>${filename}</a>`;
-    }
-
-    listing += `
-      <div>
-        ${link}
-      </div>
-    `;
-  }
-
-  const html = `
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>remFS</title>
-      </head>
-
-      <body>
-        <div style='margin: 0 auto; max-width: 640px; font-size: 24px;'>
-          ${listing}
-        </div>
-      </body>
-    </html>
-  `;
-
-  res.write(html);
-  res.end();
-}
 
 async function buildRemfsDir(fsPath) {
 
@@ -226,7 +153,6 @@ async function buildRemfsDir(fsPath) {
     res.end();
     return;
   }
-
 
   const remfs = {
     type: 'dir',
---
+++ render_html_dir.js
@@ -0,0 +1,77 @@
+async function renderHtmlDir(req, res, rootPath, reqPath, fsPath) {
+  let filenames;
+  try {
+    filenames = await fs.promises.readdir(fsPath);
+  }
+  catch (e) {
+    res.statusCode = 500;
+    res.write("Error reading directory");
+    res.end();
+    return;
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+
+  let pathParts = reqPath.split('/');
+
+  let listing;
+  if (pathParts.length > 1) {
+    pathParts.pop();
+    const parentUrl = rootPath + pathParts.join('/');
+    listing = `<a href=${parentUrl}>..[parent]</a> &#128193`;
+  }
+  else {
+    listing = '';
+  }
+
+  for (const filename of filenames) {
+
+    const url = rootPath + reqPath + '/' + filename;
+    const childFsPath = path.join(fsPath, filename);
+
+    let childStats;
+    try {
+      childStats = await fs.promises.stat(childFsPath);
+    }
+    catch (e) {
+      console.error("This one shouldn't happen");
+      console.error(e);
+      continue;
+    }
+
+    let link;
+    if (childStats.isDirectory()) {
+      link = `<a href=${url}>${filename}</a> &#128193`;
+    }
+    else {
+      link = `<a target='_blank' href=${url}>${filename}</a>`;
+    }
+
+    listing += `
+      <div>
+        ${link}
+      </div>
+    `;
+  }
+
+  const html = `
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>remFS</title>
+      </head>
+
+      <body>
+        <div style='margin: 0 auto; max-width: 640px; font-size: 24px;'>
+          ${listing}
+        </div>
+      </body>
+    </html>
+  `;
+
+  res.write(html);
+  res.end();
+}
+
+module.exports = { renderHtmlDir };
