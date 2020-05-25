ae2ce63f32e9e53da91533fdd31becf4f0e77db5
Anders Pitman <tapitman11@gmail.com>
2020-05-24 22:22:13 -0600

    rclone backend basically working
    This is madness

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const url = require('url');
 const querystring = require('querystring');
 const http = require('https');
+const { spawn } = require('child_process');
 const { renderHtmlDir } = require('./render_html_dir.js');
 const { PauthBuilder } = require('./pauth.js');
 const { parseToken, parsePath, encodePath, buildRemfsDir, getMime } = require('./utils.js');
@@ -153,11 +154,60 @@ async function createHandler(options) {
 
       if (reqPath.endsWith('remfs.json')) {
 
-        const fsPath = path.join(fsRoot, path.dirname(reqPath));
+        if (reqPath === '/remfs.json') {
+          const cmd = spawn('rclone', ['listremotes']);
+          let allData = '';
 
-        const remfs = await buildRemfsDir(fsPath);
-        res.write(JSON.stringify(remfs, null, 2));
-        res.end();
+          cmd.stdout.setEncoding('utf8');
+
+          cmd.stdout.on('data', (data) => {
+            allData += data;
+          });
+
+          cmd.stdout.on('end', () => {
+            const remotes = allData.split('\n')
+              .slice(0, -1)
+              .map(r => r.slice(0, -1));
+            console.log(remotes);
+
+            const remfs = {
+              type: 'dir',
+              size: 4096,
+              children: {},
+            };
+
+            for (const remote of remotes) {
+              remfs.children[remote] = {
+                type: 'dir',
+                size: 4096,
+              };
+            }
+
+            res.write(JSON.stringify(remfs, null, 2));
+            res.end();
+          });
+        }
+        else {
+
+          const pathParts = parsePath(reqPath);
+          console.log(pathParts);
+          const path = encodePath(pathParts.slice(0, -1));
+          try {
+            const lsResult = await rcloneLs(path);
+            res.write(JSON.stringify(rcloneDirToRemfs(lsResult), null, 2));
+          }
+          catch (e) {
+            res.statusCode = 404;
+            res.write("Not found");
+          }
+          res.end();
+        }
+
+        //const fsPath = path.join(fsRoot, path.dirname(reqPath));
+
+        //const remfs = await buildRemfsDir(fsPath);
+        //res.write(JSON.stringify(remfs, null, 2));
+        //res.end();
       }
       else {
         serveItem(req, res, fsRoot, rootPath, reqPath); 
@@ -207,56 +257,77 @@ async function serveItem(req, res, fsRoot, rootPath, reqPath) {
     console.error(e);
   });
 
-  const fsPath = path.join(fsRoot, reqPath);
+  //const fsPath = path.join(fsRoot, reqPath);
 
-  let stats
+  //let stats
+  //try {
+  //  stats = await fs.promises.stat(fsPath);
+  //}
+  //catch (e) {
+  //  res.statusCode = 404;
+  //  res.write("Not Found");
+  //  res.end();
+  //  return;
+  //}
+
+  // TODO: cache this when they first ls the path above
+  let lsResult;
   try {
-    stats = await fs.promises.stat(fsPath);
+    lsResult = await rcloneLs(reqPath);
   }
   catch (e) {
     res.statusCode = 404;
-    res.write("Not Found");
+    res.write("Not found");
     res.end();
     return;
   }
 
+  const isFile = lsResult.length === 1 && lsResult[0].Name === path.basename(reqPath);
+  const remfs = isFile ? rcloneFileToRemfs(lsResult[0]) : rcloneDirToRemfs(lsResult);
+  console.log(remfs);
+
+  if (remfs.type === 'dir') {
+    res.statusCode = 404;
+    res.write("Not found");
+    res.end();
+    return;
   // render simple html interface
-  if (stats.isDirectory()) {
-    let isWebDir = false;
-    const localRemfsPath = path.join(fsPath, 'remfs.json');
-    try {
-      const localRemfsDataText = await fs.promises.readFile(localRemfsPath, {
-        encoding: 'utf8',
-      });
-      const localRemfsData = JSON.parse(localRemfsDataText);
-      isWebDir = localRemfsData.ext.http && localRemfsData.ext.http.isWebDir;
-      redirect = localRemfsData.ext.http && localRemfsData.ext.http.redirect;
+  //if (stats.isDirectory()) {
+    //let isWebDir = false;
+    //const localRemfsPath = path.join(fsPath, 'remfs.json');
+    //try {
+    //  const localRemfsDataText = await fs.promises.readFile(localRemfsPath, {
+    //    encoding: 'utf8',
+    //  });
+    //  const localRemfsData = JSON.parse(localRemfsDataText);
+    //  isWebDir = localRemfsData.ext.http && localRemfsData.ext.http.isWebDir;
+    //  redirect = localRemfsData.ext.http && localRemfsData.ext.http.redirect;
 
-      if (redirect) {
-        res.statusCode = 307;
-        res.setHeader('Location', redirect.location);
-        res.write("Temporary Redirect");
-        res.end();
-        return;
-      }
-    }
-    catch (e) {
-      //console.log(e);
-    }
+    //  if (redirect) {
+    //    res.statusCode = 307;
+    //    res.setHeader('Location', redirect.location);
+    //    res.write("Temporary Redirect");
+    //    res.end();
+    //    return;
+    //  }
+    //}
+    //catch (e) {
+    //  //console.log(e);
+    //}
 
-    if (isWebDir) {
-      const indexPath = path.join(fsPath, 'index.html');
-      const stream = fs.createReadStream(indexPath)
-      stream.on('error', (e) => {
-        res.statusCode = 404;
-        res.write("Not Found");
-        res.end();
-      });
-      stream.pipe(res);
-    }
-    else {
-      await renderHtmlDir(req, res, rootPath, reqPath, fsPath);
-    }
+    //if (isWebDir) {
+    //  const indexPath = path.join(fsPath, 'index.html');
+    //  const stream = fs.createReadStream(indexPath)
+    //  stream.on('error', (e) => {
+    //    res.statusCode = 404;
+    //    res.write("Not Found");
+    //    res.end();
+    //  });
+    //  stream.pipe(res);
+    //}
+    //else {
+    //  await renderHtmlDir(req, res, rootPath, reqPath, fsPath);
+    //}
   }
   else {
 
@@ -270,27 +341,28 @@ async function serveItem(req, res, fsRoot, rootPath, reqPath) {
       const right = rangeHeader.split('=')[1];
       const rangeParts = right.split('-');
       range.start = Number(rangeParts[0]);
-      range.end = stats.size - 1;
+      range.end = remfs.size - 1;
 
       if (rangeParts[1]) {
         // Need to add one because HTTP ranges are inclusive
         range.end = Number(rangeParts[1]);
       }
 
-      const originalSize = stats.size;
+      const originalSize = remfs.size;
 
       res.setHeader('Content-Range', `bytes ${range.start}-${range.end}/${originalSize}`);
       res.statusCode = 206;
 
       //sendFile = sendFile.slice(range.start, range.end + 1);
-      stream = fs.createReadStream(fsPath, {
-        start: range.start,
-        end: range.end,
-      });
+      //stream = fs.createReadStream(fsPath, {
+      //  start: range.start,
+      //  end: range.end,
+      //});
+      stream = rcloneCat(reqPath, range.start, range.end + 1 - range.start);
     }
     else {
-      res.setHeader('Content-Length', `${stats.size}`);
-      stream = fs.createReadStream(fsPath);
+      res.setHeader('Content-Length', `${remfs.size}`);
+      stream = rcloneCat(reqPath);
     }
 
     res.setHeader('Accept-Ranges', 'bytes');
@@ -309,6 +381,79 @@ async function serveItem(req, res, fsRoot, rootPath, reqPath) {
   }
 }
 
+
+function rcloneDirToRemfs(rclone) {
+  const remfs = {
+    type: 'dir',
+    size: 4096,
+    children: {},
+  };
+  for (const item of rclone) {
+    remfs.children[item['Name']] = {
+      type: item['IsDir'] ? 'dir' : 'file',
+      size: item['IsDir'] ? 4096 : item['Size'],
+    };
+  }
+  return remfs;
+}
+
+function rcloneFileToRemfs(item) {
+  return {
+    type: 'file',
+    size: item['Size'],
+  };
+}
+
+async function rcloneLs(reqPath) {
+  console.log("rcloneLs", reqPath);
+  const pathParts = parsePath(reqPath);
+  const rclonePath = pathParts[0] + ':' +  encodePath(pathParts.slice(1)).slice(1);
+  const ls = spawn('rclone', ['lsjson', rclonePath]);
+
+  ls.stdout.setEncoding('utf8');
+
+  let json = '';
+
+  return new Promise((resolve, reject) => {
+    ls.stdout.on('data', (data) => {
+      console.log(data);
+      json += data;
+    });
+
+    ls.stdout.on('end', () => {
+      try {
+        const out = JSON.parse(json);
+        resolve(out);
+      }
+      catch (e) {
+        reject(e);
+      }
+    });
+  })
+}
+
+function rcloneCat(reqPath, offset, count) {
+  const pathParts = parsePath(reqPath);
+  const rclonePath = pathParts[0] + ':' +  encodePath(pathParts.slice(1)).slice(1);
+
+  const args = ['cat'];
+
+  if (offset) {
+    args.push('--offset');
+    args.push(offset);
+  }
+
+  if (count) {
+    args.push('--count');
+    args.push(count);
+  }
+
+  args.push(rclonePath);
+
+  const cat = spawn('rclone', args);
+
+  return cat.stdout;
+}
 
 async function parseBody(req) {
   return new Promise((resolve, reject) => {
